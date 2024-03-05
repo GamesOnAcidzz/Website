9b679789c3f6764c02be97a5636c1b28581fa5bd
GamesOnAcidzz <103296087+GamesOnAcidzz@users.noreply.github.com>
2024-03-05 23:09:04 +0100

    First Commit

---
+++ Theme/Theme.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor.Themes
+{
+    public abstract class Theme
+    {
+        public abstract class Colors
+        {
+            public const string Primary = "#6A3CBF";
+            public const string Secondary = "#DEDEFD";
+            public const string Tertionary = "#16003E";
+            public const string Black = "#141416";
+            public const string Highlight = "#4B2A87";
+            public const string AlmostBlack = "#24282D";
+        }
+    }
+    
+}
